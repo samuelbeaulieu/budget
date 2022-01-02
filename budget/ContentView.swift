9c4eb9b6a7da31174f30ff0ea815ded9b817a32a
Samuel Beaulieu <samuel.beaulieu28@gmail.com>
2022-01-02 17:54:30 -0500

    Reverse Transactions List
    Show the most recent transaction first

--- budget/ContentView.swift
+++ budget/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: false)],
         animation: .default)
     private var items: FetchedResults<Item>
 
