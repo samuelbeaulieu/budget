7cb2e9bd8e5f6f9c270d229100b4f05e1e2bf313
Samuel Beaulieu <samuel.beaulieu28@gmail.com>
2022-01-16 20:42:00 -0500

    Category Model

---
+++ budget/Category+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Category+CoreDataClass.swift
+//  budget
+//
+//  Created by Samuel Beaulieu on 2022-01-16.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Category)
+public class Category: NSManagedObject {
+
+}
---
+++ budget/Category+CoreDataProperties.swift
@@ -0,0 +1,40 @@
+//
+//  Category+CoreDataProperties.swift
+//  budget
+//
+//  Created by Samuel Beaulieu on 2022-01-16.
+//
+//
+
+import Foundation
+import CoreData
+import UIKit.UIColor
+
+
+extension Category: Identifiable {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Category> {
+        return NSFetchRequest<Category>(entityName: "Category")
+    }
+
+    @NSManaged public var foregroundColor: UIColor
+    @NSManaged public var iconName: String
+    @NSManaged public var id: UUID?
+    @NSManaged public var name: String
+    @NSManaged public var type: Int32
+
+}
+
+enum CategoryType: Int32, CaseIterable {
+    case income
+    case expense
+    
+    var displayString: String {
+        switch self {
+        case .expense:
+            return "Expenses"
+        case .income:
+            return "Income"
+        }
+    }
+}
