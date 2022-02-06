aff9f492b88223a9b85c33bd563f24be98a08b47
Samuel Beaulieu <samuel.beaulieu28@gmail.com>
2022-02-06 02:44:49 -0500

    Filter TransactionList by month, Add Income/Expenses/Balance Widget

--- budget/TransactionList.swift
+++ budget/TransactionList.swift
@@ -18,10 +18,15 @@ struct TransactionList: View {
 
     @State private var showingSheet = false
     @State private var showingAddSheet = false
-
-    func update(_ result: FetchedResults<Transaction>) -> [[Transaction]] {
+    
+    var filteredTransactions: [Transaction] {
+        return transactions.filter { Calendar.current.isDateInMonth($0.timestamp, currentMonth) }
+    }
+    
+    func update(_ result: [Transaction]) -> [[Transaction]] {
         return Dictionary(grouping: result) { (element: Transaction) in
             Calendar.current.startOfDay(for: element.timestamp)
+//            Calendar.current.isDateInThisMonth(element.timestamp)
         }.values.sorted() {
             $0[0].timestamp > $1[0].timestamp
         }
@@ -37,10 +42,83 @@ struct TransactionList: View {
         return total
     }
 
+    func getBalance(_ section: [Transaction]) -> NSDecimalNumber {
+        var total = NSDecimalNumber(decimal: 0)
+        for transaction in section {
+            if transaction.type == TransactionType.expense.rawValue {
+                total = total.subtracting(transaction.amount)
+            }
+            if transaction.type == TransactionType.income.rawValue {
+                total = total.adding(transaction.amount)
+            }
+        }
+        return total
+    }
+
+    @State private var currentMonth = 0
+    @State private var month = Calendar.current.date(byAdding: .month, value: 0, to: Date())
+
     var body: some View {
         NavigationView {
             List {
-                ForEach(update(transactions), id: \.self) { (section: [Transaction]) in
+                Section {
+                    HStack(alignment: .center) {
+                        VStack() {
+                            Text(getTotalSpent(update(filteredTransactions).first ?? [], type: TransactionType.income), formatter: currencyFormatter)
+                                .padding(.bottom, 1)
+                            Text("Income")
+                        }
+                        Spacer()
+                        Divider()
+                        Spacer()
+                        VStack() {
+                            Text(getTotalSpent(update(filteredTransactions).first ?? [], type: TransactionType.expense), formatter: currencyFormatter)
+                                .padding(.bottom, 1)
+                            Text("Expenses")
+                        }
+                        Spacer()
+                        Divider()
+                        Spacer()
+                        VStack() {
+                            Text(getBalance(update(filteredTransactions).first ?? []), formatter: currencyFormatter)
+                                .padding(.bottom, 1)
+                            Text("Balance")
+                        }
+                    }
+                    .padding()
+                }
+                Section {
+                    HStack() {
+                        Button {
+                            let impactMed = UIImpactFeedbackGenerator(style: .light)
+                            impactMed.impactOccurred()
+                            currentMonth = currentMonth - 1
+                        } label: {
+                            Label("Previous Month", systemImage: "chevron.left")
+                                .labelStyle(.iconOnly)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.blue)
+                        .onChange(of: currentMonth) { newValue in
+                            month = Calendar.current.date(byAdding: .month, value: newValue, to: Date())
+                        }
+                        Spacer()
+                        Text(month!, formatter: monthDateFormatter)
+                        Spacer()
+                        Button {
+                            let impactMed = UIImpactFeedbackGenerator(style: .light)
+                            impactMed.impactOccurred()
+                            currentMonth = currentMonth + 1
+                        } label: {
+                            Label("Next Month", systemImage: "chevron.right")
+                                .labelStyle(.iconOnly)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(.blue)
+                    }
+                    .padding(.horizontal)
+                }
+                ForEach(update(filteredTransactions), id: \.self) { (section: [Transaction]) in
                     Section(header: (
                         HStack() {
                             Text(section[0].timestamp, formatter: mediumDateFormatter)
@@ -76,7 +154,7 @@ struct TransactionList: View {
                                             Label("Edit Transaction", systemImage: "pencil")
                                         }
                                         Button(role: .destructive) {
-                                            if let index = transactions.firstIndex(of: transaction) {
+                                            if let index = filteredTransactions.firstIndex(of: transaction) {
                                                 deleteItems(section: section, offsets: [index])
                                             }
                                         } label: {
@@ -88,7 +166,7 @@ struct TransactionList: View {
                         .onDelete { indexSet in
                             deleteItems(section: section, offsets: indexSet)
                         }
-                        if transactions.isEmpty {
+                        if filteredTransactions.isEmpty {
                             Text("No transactions")
                         }
                     }
@@ -104,7 +182,7 @@ struct TransactionList: View {
                     }
                 }
                 ToolbarItem {
-                    if !transactions.isEmpty {
+                    if !filteredTransactions.isEmpty {
                         EditButton()
                     }
                 }
@@ -158,6 +236,12 @@ let mediumDateFormatter: DateFormatter = {
     return formatter
 }()
 
+let monthDateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "MMMM YYYY"
+    return formatter
+}()
+
 let currencyFormatter: NumberFormatter = {
     let formatter = NumberFormatter()
     formatter.numberStyle = .currency
@@ -167,5 +251,60 @@ let currencyFormatter: NumberFormatter = {
 struct TransactionList_Previews: PreviewProvider {
     static var previews: some View {
         TransactionList().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
+struct ControlGroupWithTitle: ControlGroupStyle {
+    let title: LocalizedStringKey
+
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            Text(title)
+                .font(.title)
+            HStack {
+                configuration.content
+            }
+        }
+    }
+}
+
+
+extension Calendar {
+  private var currentDate: Date { return Date() }
+
+  func isDateInThisWeek(_ date: Date) -> Bool {
+    return isDate(date, equalTo: currentDate, toGranularity: .weekOfYear)
+  }
+
+  func isDateInThisMonth(_ date: Date) -> Bool {
+    return isDate(date, equalTo: currentDate, toGranularity: .month)
+  }
+
+  func isDateInNextWeek(_ date: Date) -> Bool {
+    guard let nextWeek = self.date(byAdding: DateComponents(weekOfYear: 1), to: currentDate) else {
+      return false
+    }
+    return isDate(date, equalTo: nextWeek, toGranularity: .weekOfYear)
+  }
+
+  func isDateInNextMonth(_ date: Date) -> Bool {
+    guard let nextMonth = self.date(byAdding: DateComponents(month: 1), to: currentDate) else {
+      return false
+    }
+    return isDate(date, equalTo: nextMonth, toGranularity: .month)
+  }
+
+  func isDateInFollowingMonth(_ date: Date) -> Bool {
+    guard let followingMonth = self.date(byAdding: DateComponents(month: 2), to: currentDate) else {
+      return false
+    }
+    return isDate(date, equalTo: followingMonth, toGranularity: .month)
+  }
+    
+    func isDateInMonth(_ date: Date, _ month: Int) -> Bool {
+      guard let followingMonth = self.date(byAdding: DateComponents(month: month), to: currentDate) else {
+        return false
+      }
+      return isDate(date, equalTo: followingMonth, toGranularity: .month)
     }
 }
